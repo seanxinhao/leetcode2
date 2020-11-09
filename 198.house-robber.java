5d44f2c647fd6e91764d7dd0a45753c3f74cfaf5
Xin Hao <xinhao@macbook-pro.lan>
2020-11-08 20:29:11 -0800

    1 more problem house-robber.

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -11,16 +11,15 @@ class Solution {
         if (n == 0) {
             return 0;
         }
-        int[][] dp = new int[2][n];
-        dp[0][0] = 0;
-        dp[1][0] = nums[0];
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = Math.max(dp[0][i - 1], dp[1][i - 1]);
-            dp[1][i] = dp[0][i - 1] + nums[i];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
 
-        return Math.max(dp[0][n - 1], dp[1][n - 1]);
+        return dp[n];
     }
 }
 // @lc code=end
