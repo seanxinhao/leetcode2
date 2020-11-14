dd96f4b7bb61d050e69cfa0098df1107eee21792
Xin Hao <xinhao@macbook-pro.lan>
2020-11-13 22:02:27 -0800

    2020/11/13 3 problems two lists DP.

---
+++ 718.maximum-length-of-repeated-subarray.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=718 lang=java
+ *
+ * [718] Maximum Length of Repeated Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int findLength(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int[][] dp = new int[m + 1][n + 1];
+
+        int max = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+
+        return max;
+    }
+}
+// @lc code=end
